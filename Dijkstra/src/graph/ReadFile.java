8e91dca8faf0db2e4ff0df81eb7e79066c924c68
Hawkeye281 <gen.hawkeye@gmx.de>
2013-07-03 19:22:58 +0200

    Es wird eine NumberFormatException geworfen, vielleicht verstehst du warum. Bin da planlos.

--- Dijkstra/src/graph/ReadFile.java
+++ Dijkstra/src/graph/ReadFile.java
@@ -3,23 +3,37 @@ package graph;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class ReadFile {
 	private int maxNodes = 4;
 	private boolean read = true;
 	private String zeile;
+	private WeightedGraph wg;
 	
 	public void readFile() throws IOException 
 	  {
 	    FileReader fr = new FileReader("Graph.txt");
 	    BufferedReader br = new BufferedReader(fr);
+	    HashMap<String, Integer> dist = new HashMap<String, Integer>();
+	    wg = new WeightedGraph(3);
 
 	    while ((zeile = br.readLine()) != null){
 	    	
 		    if (zeile.equals("Node")){
 		    	for(int i=0; i<maxNodes;i++){
 		    		String row = br.readLine();
-		    		System.out.println(row);
+		    		//System.out.println(row);
+		    		String [] splitResult = row.split(", ");
+		    		//System.out.println(splitResult[0]);
+		    		//System.out.println(splitResult[1]);
+		    		if (splitResult[1].equals("0")){
+		    			wg.addNode(splitResult[0], dist);
+		    		} else if (splitResult[1].equals("-1")){
+		    			System.out.println(splitResult[1]);
+		    		} else {
+		    			dist.put(splitResult[0], Integer.parseInt(splitResult[1]));
+		    		}
 		    		
 		    	}
 		    } 
